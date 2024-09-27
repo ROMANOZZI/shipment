2e5106932337d3e81c5d7b7b1b1c5951479f352b
Mohamed Salah <78039872+ROMANOZZI@users.noreply.github.com>
2024-09-27 09:59:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen w-full flex bg-cyan-500 text-center justify-center items-center">
+      <p className="text-3xl text-center font-bold">
+        Hello world!
+      </p>
+    </div>
+  );
+}
